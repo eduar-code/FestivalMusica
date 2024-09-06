bc328b45aaa0993fc875a44c56f1efbd2107d28c
Alexis Eduardo <eduardo.7493z@gmail.com>
2024-09-05 23:45:06 -0600

    Added effects of nav with js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+    navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 });
+
+function navegacionFija() {
+    const header = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    document.addEventListener('scroll', function () {
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+
+    })
+
+}
 
 function crearGaleria() {
     const CANTIDAD_IMAGENES = 16;
@@ -29,14 +47,14 @@ function mostrarImagen(i) {
     const modal = document.createElement('DIV');
     modal.classList.add('modal');
     modal.onclick = cerrarModal;
-   
+
     //Boton cerrar modal
-    const cerrarModalBtn = document.createElement('BUTTON');    
+    const cerrarModalBtn = document.createElement('BUTTON');
     cerrarModalBtn.textContent = 'X';
     cerrarModalBtn.classList.add('btn-cerrar');
     cerrarModalBtn.onclick = cerrarModal;
 
-    
+
     modal.appendChild(imagen);
     modal.appendChild(cerrarModalBtn);
 
@@ -57,4 +75,40 @@ function cerrarModal() {
         body.classList.remove('overflow-hidden');
     }, 500);
 
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function () {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+                actual = section.id;
+            }
+        });
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active');
+            }
+        });
+    });
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+            section.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
 }
