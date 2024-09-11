798dccd6e2cecaf9d12c2438764a2f2dc3c56105
Alexis Eduardo <eduardo.7493z@gmail.com>
2024-09-10 19:59:28 -0600

    Added performance Web

--- src/js/app.js
+++ src/js/app.js
@@ -25,9 +25,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = "Imagen Galeria"
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
 
         //Event Handler
         imagen.onclick = function () {// usar funtion cuando mandas parametros
@@ -39,9 +42,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = "Imagen Galeria"
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
 
     //General modal
     const modal = document.createElement('DIV');
